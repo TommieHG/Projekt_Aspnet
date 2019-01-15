2830eed8289e57366057ceda2a31915d93f1edc8
tommieg <tommieg@kth.se>
2019-01-15 08:45:30 +0100

    First
    Stuff

---
+++ Inventory/Inventory/Controllers/ObjectController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Inventory.Models;
+
+namespace Inventory.Controllers
+{
+    public class ObjectController : Controller
+    {
+        private InventoryEntities db = new InventoryEntities();
+
+        // GET: Object
+        public ActionResult Index()
+        {
+            var tbl_Object = db.Tbl_Object.Include(t => t.Tbl_Category);
+            return View(tbl_Object.ToList());
+        }
+
+        // GET: Object/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tbl_Object tbl_Object = db.Tbl_Object.Find(id);
+            if (tbl_Object == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tbl_Object);
+        }
+
+        // GET: Object/Create
+        public ActionResult Create()
+        {
+            ViewBag.FK_Ca_ID = new SelectList(db.Tbl_Category, "Ca_ID", "Ca_Name");
+            return View();
+        }
+
+        // POST: Object/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Ob_ID,FK_Ca_ID,Ob_Name,Ob_Purchase_Date,Ob_Est_Value,Ob_Description")] Tbl_Object tbl_Object)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Tbl_Object.Add(tbl_Object);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.FK_Ca_ID = new SelectList(db.Tbl_Category, "Ca_ID", "Ca_Name", tbl_Object.FK_Ca_ID);
+            return View(tbl_Object);
+        }
+
+        // GET: Object/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tbl_Object tbl_Object = db.Tbl_Object.Find(id);
+            if (tbl_Object == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.FK_Ca_ID = new SelectList(db.Tbl_Category, "Ca_ID", "Ca_Name", tbl_Object.FK_Ca_ID);
+            return View(tbl_Object);
+        }
+
+        // POST: Object/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Ob_ID,FK_Ca_ID,Ob_Name,Ob_Purchase_Date,Ob_Est_Value,Ob_Description")] Tbl_Object tbl_Object)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tbl_Object).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.FK_Ca_ID = new SelectList(db.Tbl_Category, "Ca_ID", "Ca_Name", tbl_Object.FK_Ca_ID);
+            return View(tbl_Object);
+        }
+
+        // GET: Object/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tbl_Object tbl_Object = db.Tbl_Object.Find(id);
+            if (tbl_Object == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tbl_Object);
+        }
+
+        // POST: Object/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Tbl_Object tbl_Object = db.Tbl_Object.Find(id);
+            db.Tbl_Object.Remove(tbl_Object);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ SQLQuery4.sql
@@ -0,0 +1,40 @@
+﻿CREATE TABLE Tbl_Category (
+	Ca_ID INT IDENTITY (1,1) NOT NULL,
+	Ca_Name NVARCHAR (50) NOT NULL,
+	CONSTRAINT PK_Tbl_Category PRIMARY KEY CLUSTERED (Ca_ID ASC)
+);
+
+CREATE TABLE Tbl_Location (
+	Lo_ID INT IDENTITY (1,1) NOT NULL,
+	Lo_Name NVARCHAR (50) NOT NULL,
+	CONSTRAINT PK_Tbl_Location PRIMARY KEY CLUSTERED (Lo_ID ASC)
+);
+
+CREATE TABLE Tbl_Object (
+	Ob_ID INT IDENTITY (1,1) NOT NULL,
+	FK_Ca_ID INT NOT NULL,
+	Ob_Name NVARCHAR (50) NOT NULL,
+	Ob_Purchase_Date DATE,
+	Ob_Est_Value FLOAT,
+	Ob_Description NVARCHAR (280)
+	CONSTRAINT PK_Tbl_Object PRIMARY KEY CLUSTERED (Ob_ID ASC),
+	FOREIGN KEY (FK_Ca_ID) REFERENCES Tbl_Category (Ca_ID)
+);
+
+CREATE TABLE Tbl_Object_Location (
+    Ob_ID INT NOT NULL,
+    Lo_ID  INT NOT NULL,
+	Qty    INT NOT NULL,
+    CONSTRAINT [PK_Object_Location] PRIMARY KEY CLUSTERED ([Ob_ID] ASC, [Lo_ID] ASC),
+    FOREIGN KEY ([Ob_ID]) REFERENCES [Tbl_Object] ([Ob_Id]),
+    FOREIGN KEY ([Lo_ID]) REFERENCES [Tbl_Location] ([Lo_ID])
+);
+
+INSERT INTO Tbl_Location (Lo_Name)
+VALUES ('Vardagsrum'),('Sovrum'),('Badrum'),('Kök'),('Förråd');
+
+INSERT INTO Tbl_Category (Ca_Name)
+VALUES ('Stolar'),('Bord'),('Elektronik'),('Förvaring'),('Textilier'),('Kläder');
+
+INSERT INTO Tbl_Object (FK_Ca_ID, Ob_Name, Ob_Purchase_Date, Ob_Est_Value, Ob_Description)
+VALUES (1, 'Pinnstol', '2017-03-04', NULL, NULL),(2, 'Matbord', NULL, 1500, 'Det där man äter mat'), (3, 'TV', '2018-10-30', 10000, 'En dumburk');
